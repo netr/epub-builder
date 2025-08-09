81de07212fc7448447b3d5824a50e29c57200be9
netr <programmatical@gmail.com>
2025-08-08 19:56:26 -0700

    v2

--- examples/accessibility.rs
+++ examples/accessibility.rs
@@ -97,7 +97,12 @@ img {
 
     // Add cover image with alt text consideration
     // builder.add_cover_image("cover.png", dummy_image.as_bytes(), "image/png")?;
-    builder.add_resource_with_id("cover.png", dummy_image.as_bytes(), "image/png", "cover-image")?;
+    builder.add_resource_with_id(
+        "cover.png",
+        dummy_image.as_bytes(),
+        "image/png",
+        "cover-image",
+    )?;
 
     // Add the main content
     builder
